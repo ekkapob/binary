81f3213e1c084d772a4d1781a0d217f646f3af6e
Ekkapob <ekkapob@gmail.com>
2014-02-09 13:27:11 +0700

    Update index

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,13 @@
+var products = [];
+
+products.push({
+	name: 'Navy Blue Set',
+	id: 'FAM0902141',
+	description: 'ชุดน้ำงินเข้าเซ๊ทใส่สบาย เหมาะสำหรับวัดหยุดพักผ่อน ไปเดินเล่นในสวนสาธาระณะ แบบเรียบง่าย',
+	created: 1391926601273,
+	images: ['FAM0902141_001.jpg']
+});
+
 exports.index = function(req, res) {
 	res.render('index', {
 		flash: {
